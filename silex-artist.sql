07bdf317e85605e6f88a7bed29d90ade1a7aa447
Reelwens <sim.lucas27@gmail.com>
2017-05-31 09:34:33 +0200

    :construction: Update sql

--- silex-artist.sql
+++ silex-artist.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Client :  localhost:3306
--- Généré le :  Sam 27 Mai 2017 à 17:10
--- Version du serveur :  5.6.35
--- Version de PHP :  7.0.15
+-- Client :  127.0.0.1
+-- Généré le :  Mer 31 Mai 2017 à 07:32
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -61,16 +61,48 @@ CREATE TABLE `songs` (
   `link` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `songs`
+--
+
+INSERT INTO `songs` (`id`, `slug`, `name`, `time`, `link`) VALUES
+(1, 'dead_inside', 'Dead Inside', '4:23', 'https://www.youtube.com/watch?v=aTcJWhmdzpg'),
+(2, 'psycho', 'Psycho', '5:16', 'https://www.youtube.com/watch?v=UqLRqzTp6Rk'),
+(3, 'mercy', 'Mercy', '3:51', 'https://www.youtube.com/watch?v=yj8Xpdx60Ws'),
+(4, 'the_handler', 'The Handler', '4:33', 'https://www.youtube.com/watch?v=BF1DQr5dKW8'),
+(5, 'supremacy', 'Supremacy', '4:55', 'https://www.youtube.com/watch?v=avM_UsVo0IA'),
+(6, 'uprising', 'Uprising', '5:04', 'https://www.youtube.com/watch?v=w8KQmps-Sog'),
+(7, 'take_a_bow', 'Take a Bow', '4:35', 'https://www.youtube.com/watch?v=wQXit0vly2I'),
+(8, 'apocalypse_please', 'Apocalypse Please', '4:12', 'https://www.youtube.com/watch?v=YFrl01aPPkA'),
+(9, 'new_born', 'New Born', '6:03', 'https://www.youtube.com/watch?v=qhduQhDqtb4'),
+(10, 'sunburn', 'Sunburn', '3:54', 'https://www.youtube.com/watch?v=N9SZaOJEWXU');
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `songs_types`
+-- Structure de la table `songs_albums`
 --
 
-CREATE TABLE `songs_types` (
-  `id_albums` int(11) NOT NULL,
-  `id_songs` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `songs_albums` (
+  `id_songs` int(11) NOT NULL,
+  `id_albums` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `songs_albums`
+--
+
+INSERT INTO `songs_albums` (`id_songs`, `id_albums`) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 2),
+(6, 3),
+(7, 4),
+(8, 5),
+(9, 6),
+(10, 7);
 
 --
 -- Index pour les tables exportées
@@ -89,13 +121,6 @@ ALTER TABLE `songs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `songs_types`
---
-ALTER TABLE `songs_types`
-  ADD UNIQUE KEY `id_albums` (`id_albums`),
-  ADD UNIQUE KEY `id_songs` (`id_songs`);
-
---
 -- AUTO_INCREMENT pour les tables exportées
 --
 
@@ -108,7 +133,7 @@ ALTER TABLE `albums`
 -- AUTO_INCREMENT pour la table `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
